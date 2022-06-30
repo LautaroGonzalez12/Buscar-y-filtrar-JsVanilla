cf72af4688dbf7d2723150882a140ada48cd218d
Lautaro Gonzalez <lautaro.gonzalez.dev@gmail.com>
2022-06-30 16:37:53 -0300

    Se agrego addEventListeners a los selects

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 //variables
-const results = document.getElementById('resultado');
+const marca = document.getElementById('marca');
 const year = document.getElementById('year');
+const min = document.getElementById('minimo');
+const max = document.getElementById('maximo');
+const puertas = document.getElementById('puertas');
+const transmision = document.getElementById('transmision');
+const color = document.getElementById('color');
+
+const results = document.getElementById('resultado');
 const maxYear = new Date().getFullYear();
 const minYear = maxYear - 10;
+
+const dataSearch = {
+    marca: "",
+    year: "",
+    minimo:"",
+    maximo:"",
+    puertas:"",
+    transmision:"",
+    color:"",
+}
 
 //Principal function
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,6 +27,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     fillSelects();
 });
+//event listeners
+marca.addEventListener('change', (e) => {
+   dataSearch.marca = e.target.value;
+})
+year.addEventListener('change', (e) => {
+   dataSearch.year = e.target.value;
+})
+min.addEventListener('change', (e) => {
+   dataSearch.minimo = e.target.value;
+})
+maximo.addEventListener('change', (e) => {
+   dataSearch.maximo = e.target.value;
+})
+puertas.addEventListener('change', (e) => {
+   dataSearch.puertas = e.target.value;
+})
+transmision.addEventListener('change', (e) => {
+   dataSearch.transmision = e.target.value;
+})
+color.addEventListener('change', (e) => {
+   dataSearch.color = e.target.value;
+})
 
 //functions 
 function showCars(){
@@ -22,7 +61,7 @@ function showCars(){
         results.appendChild(carHTML);
     });
 }
-
+//fill the selects in the html
 function fillSelects(){
 
     for(let i= maxYear; i > minYear; i--){
